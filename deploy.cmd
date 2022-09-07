bf286aac40c308d09516d66a76fc727eda362089
Alexey ALERT Rubasheff <alexey.rubasheff@gmail.com>
2022-09-07 18:02:24 +0300

    ! deploy.cmd prettifying

--- deploy.cmd
+++ deploy.cmd
@@ -8,11 +8,22 @@ set BUILD_PATH=.
 
 set DOCKER_SERVICE=com.docker.service
 set DOCKER_EXE=docker
-where %DOCKER_EXE% >nul || set "DOCKER_EXE=%ProgramFiles%\Docker\Docker\resources\bin\docker.exe"
+where %DOCKER_EXE% >nul || (
+    set "DOCKER_EXE=%ProgramFiles%\Docker\Docker\resources\bin\docker.exe"
+)
 
 pushd %~dp0
-sc query %DOCKER_SERVICE% | findstr /IC:"running" >nul || sudo net start %DOCKER_SERVICE% || echo "Error starting docker service %DOCKER_SERVICE% & exit /b
-tasklist | findstr /IC:"Docker Desktop.exe" >nul || start "" "%ProgramFiles%\Docker\Docker\Docker Desktop.exe" & timeout /t 60
+sc query %DOCKER_SERVICE% | findstr /IC:"running" >nul || (
+    sudo net start %DOCKER_SERVICE% || (
+        echo "Error starting docker service %DOCKER_SERVICE%
+        exit /b
+    )
+)
+
+tasklist | findstr /IC:"Docker Desktop.exe" >nul || (
+    start "" "%ProgramFiles%\Docker\Docker\Docker Desktop.exe"
+    timeout /t 60
+)
 
 "%DOCKER_EXE%" build -t %BUILD_TAG% %BUILD_PATH% || exit /b
 "%DOCKER_EXE%" push %REGISTRY_IP%:%REGISTRY_PORT%/%IMAGE_NAME% || exit /b
