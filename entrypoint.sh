e60937c8c90b8f42fa4e914a0633b1a1d2304092
Alexey ALERT Rubasheff <alexey.rubasheff@gmail.com>
2023-11-20 12:15:54 +0200

    ! pendulum version bugfix

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "start rozetka @ $(pwd)"
-python -V
-# python -c "from rozetka.runners.parse_api import main; main()"
-python -m "rozetka.runners.parse_api"
-echo "done rozetka"
-# cron -f
-# echo "rozetka cron done"
