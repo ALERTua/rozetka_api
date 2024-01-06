b7cfdcd7671e2b3ea0f1a71e57d941ef6c529c5f
Alexey Rubaseff <alexey.rubasheff@gmail.com>
2024-01-06 17:19:34 +0200

    ! remote docker deploy

--- deploy.cmd
+++ deploy.cmd
@@ -1,37 +1,41 @@
 @echo off
-set REGISTRY_IP=192.168.1.3
-set REGISTRY_PORT=5001
+set DOCKER_BUILDKIT=1
+set DOCKER_REGISTRY=registry.alertua.duckdns.org
 set IMAGE_NAME=rozetka
 set IMAGE_TAG=latest
-set BUILD_TAG=%REGISTRY_IP%:%REGISTRY_PORT%/%IMAGE_NAME%:%IMAGE_TAG%
+set BUILD_TAG=%DOCKER_REGISTRY%/%IMAGE_NAME%:%IMAGE_TAG%
 set BUILD_PATH=.
+rem set DOCKER_OPTS=--insecure-registry=%DOCKER_REGISTRY%
+set DOCKER_OPTS=--max-concurrent-uploads=10 --max-concurrent-downloads=10
 
-set DOCKER_SERVICE=com.docker.service
-set DOCKER_EXE=docker
-where %DOCKER_EXE% >nul || (
-    set "DOCKER_EXE=%ProgramFiles%\Docker\Docker\resources\bin\docker.exe"
-)
+if not defined DOCKER_REMOTE (
+    set DOCKER_SERVICE=com.docker.service
+    set DOCKER_EXE=docker
+    where %DOCKER_EXE% >nul || (
+        set "DOCKER_EXE=%ProgramFiles%\Docker\Docker\resources\bin\docker.exe"
+    )
 
-pushd %~dp0
-sc query %DOCKER_SERVICE% | findstr /IC:"running" >nul || (
-    echo starting Docker service %DOCKER_SERVICE%
-    sudo net start %DOCKER_SERVICE% || (
-        echo "Error starting docker service %DOCKER_SERVICE%
-        exit /b
+    pushd %~dp0
+    sc query %DOCKER_SERVICE% | findstr /IC:"running" >nul || (
+        echo starting Docker service %DOCKER_SERVICE%
+        sudo net start %DOCKER_SERVICE% || (
+            echo "Error starting docker service %DOCKER_SERVICE%
+            exit /b
+        )
+    )
+
+    tasklist | findstr /IC:"Docker Desktop.exe" >nul || (
+        start "" "%ProgramFiles%\Docker\Docker\Docker Desktop.exe"
+        :loop
+            call docker info >nul 2>nul || (
+                timeout /t 1 >nul
+                goto loop
+            )
+        rem timeout /t 60
     )
 )
 
-tasklist | findstr /IC:"Docker Desktop.exe" >nul || (
-    start "" "%ProgramFiles%\Docker\Docker\Docker Desktop.exe"
-    :loop
-        call docker info >nul 2>nul || (
-            timeout /t 1 >nul
-            goto loop
-        )
-    rem timeout /t 60
-)
-
 "%DOCKER_EXE%" build -t %BUILD_TAG% %BUILD_PATH% || exit /b
-"%DOCKER_EXE%" push %REGISTRY_IP%:%REGISTRY_PORT%/%IMAGE_NAME% || exit /b
+"%DOCKER_EXE%" push %DOCKER_REGISTRY%/%IMAGE_NAME% || exit /b
 
 @REM net stop %DOCKER_SERVICE% || exit /b
