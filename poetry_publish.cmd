32991c7a8c9edd8ac7578ce1d49fd73f0012f3c4
Alexey Rubaseff <alexey.rubasheff@gmail.com>
2024-01-06 12:09:36 +0200

    ! publish script auth check

--- poetry_publish.cmd
+++ poetry_publish.cmd
@@ -2,5 +2,8 @@
 @echo off
 pushd %~dp0
 rem call .venv\scripts\activate
-poetry config repositories.test-pypi https://test.pypi.org/legacy/
-poetry publish -r test-pypi --build
+set PYPI_REPO=test-pypi
+set PYPI_REPO_FULL=repositories.%PYPI_REPO%
+
+poetry config %PYPI_REPO_FULL% https://test.pypi.org/legacy/
+poetry publish -r %PYPI_REPO% --build || echo Please auth in %PYPI_REPO_FULL% using poetry config poetry config http-basic.%PYPI_REPO% __token__ token
